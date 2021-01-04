3560da9631a758256446f5501482a378fe35d958
tjx158589 <1464695642@qq.com>
2021-01-04 23:40:34 +0800

    sy5 1.0

--- src/sy5/no1/LoginDemo.java
+++ src/sy5/no1/LoginDemo.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 public class LoginDemo{
     public static void main(String[] args) {
-        System.out.println("请输入用户名和密码：(name pwd):");
+
         Scanner scanner=new Scanner(System.in);
 
-        String name=scanner.next();
-        String pwd=scanner.next();
+
 
         HashMap<String,User> users =new HashMap<String,User>();
         users.put("tom",new User(1,"tom","123"));
@@ -19,24 +18,28 @@ public class LoginDemo{
         users.put("robin",new User(4,"robin","123"));
 
 
+        while (true){
+            System.out.println("请输入用户名和密码：(name pwd):");
+            String name=scanner.next();
+            String pwd=scanner.next();
+            String str=name+" "+pwd;
+            System.out.println(str);
 
-        String str=name+" "+pwd;
-        System.out.println(str);
-
-        if(!str.matches("\\w+\\s\\W+")){
-            System.out.println("输入正确");
-            if(users.containsKey(name)){
-                System.out.println("用户名存在");
-                User user = users.get(name);
-                if (user.getPwd().equals(pwd)){
-                    System.out.println("热烈欢迎"+user);
-                }
+            if(!str.matches("\\w+\\s\\W+")){
+                System.out.println("输入正确");
+                if(users.containsKey(name)){
+                    System.out.println("用户名存在");
+                    User user = users.get(name);
+                    if (user.getPwd().equals(pwd)){
+                        System.out.println("热烈欢迎"+user);
+                    }
+                };
+            }
+            else {
+                System.out.println("输入不合法");
             };
-        }
-        else {
-            System.out.println("输入不合法");
-        };
 
+        }
 
     }
 }
