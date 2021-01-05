bfbe27ce1ed9f29cec1cf21ec7cd20c69e04e3fc
tjx158589 <1464695642@qq.com>
2021-01-05 09:16:24 +0800

    sy5 2.0

---
+++ src/io/demo.java
@@ -0,0 +1,12 @@
+package io;
+
+import java.io.*;
+
+public class demo {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        OutputStreamWriter out = new OutputStreamWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("ee.txt")),"utf-8");
+    }
+
+}
